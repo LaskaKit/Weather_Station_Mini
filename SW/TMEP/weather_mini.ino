be8b1e13ee06f77d09e18a7cc79118d94d295e45
Michal Ševčík <multi@tricker.cz>
2023-03-02 20:29:48 +0100

    Actualized example
    - Measurement every 5 minutes (was 15)
    - BME280 on 0x77 (was 0x76)
    - No need for my_config.h - just fill serverName here and you are good to go.

--- SW/TMEP/weather_mini.ino
+++ SW/TMEP/weather_mini.ino
@@ -1,32 +1,36 @@
-/* LasKKit DIY Mini Weather Station. 
- * TMEP edition
+/*
+ * LaskaKit DIY Mini Weather Station - TMEP edition
  * Read Temperature, Humidity and pressure and send to TMEP.cz
- * 
+ *
  * For settings see config.h
- * 
- * Email:podpora@laskakit.cz
- * Web:laskakit.cz
- * Board: ESP32-C3 Dev Module
- * 
+ *
+ * E-mail: podpora@laskakit.cz
+ * Web: laskakit.cz
+ * Board: LaskaKit Meteo Mini - ESP32-C3 Dev Module
+ * Sensor: LaskaKit BME280
  */
 
-// připojení knihoven
-#include "config_my.h"            // change to config.h and fill the file.
+// EN: Fill in your sensor domain, preferably "Domain for sending values" to be more secure, without http://
+// CZ: Vypln tvou domenu u cidla, ktere sis zalozil na tmep.cz,
+// zvol domenu pro zasilani hodnot pro lepsi zabezpeceni, uved ji bez http://
+String serverName = "DOMENA.tmep.cz";
 
+// Libraries
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>      // BME280 by Adafruit 
-#include <ESP32AnalogRead.h>      // ESP32AnalogRead by madhephaestus https://github.com/madhephaestus/ESP32AnalogRead 
+#include <Adafruit_BME280.h>      // BME280 by Adafruit
+#include <ESP32AnalogRead.h>      // ESP32AnalogRead by madhephaestus https://github.com/madhephaestus/ESP32AnalogRead
 #include <WiFiManager.h>          // WiFi manager by tzapu https://github.com/tzapu/WiFiManager
 
-#define BME280_ADDRESS (0x76)     // (0x77) cut left and solder right pad on board
-#define SLEEP_SEC 15*60           // Measurement interval (seconds)
+#define BME280_ADDRESS (0x77)     // 0x77 - cut left and solder right pad on board, otherwise it will be 0x76
+#define SLEEP_SEC 5*60            // Measurement interval (seconds) - 5 minutes - you can adjust to higher interval
+                                  // if battery is getting drained quicker than you can charge it
 #define ADC_PIN 0                 // ADC pin on LaskaKit Meteo mini
 #define deviderRatio 1.7693877551 // Voltage devider ratio on ADC pin 1M + 1.3MOhm
 
-// Vytvoření instance | Instance creation
+// Instance creation
 Adafruit_BME280 bme;
 ESP32AnalogRead adc;
 
@@ -39,35 +43,34 @@ void postData(){
 
   if(WiFi.status()== WL_CONNECTED) {
     HTTPClient http;
-      
-    //GUID, nasleduje hodnota teploty, pro vlhkost "humV", pro CO2 "CO2", pro napeti baterie "v"
-    String serverPath = serverName + "" + GUID + "=" + temperature + "&humV=" + humidity + "&pressV=" + pressure + "&v=" + bat_voltage; 
-      
-    // zacatek http spojeni
+
+    // Variables with measurements are send to server of your choice - temperature, humidity, pressure, battery voltage and Wi-Fi signal strength
+    String serverPath = "http://" + serverName + "/index.php?temp=" + temperature + "&humV=" + humidity + "&pressV=" + pressure + "&v=" + bat_voltage + "&rssi=" + WiFi.RSSI();
+
+    // begin of HTTP connection
     http.begin(serverPath.c_str());
-      
+
     // http get request
     int httpResponseCode = http.GET();
-      
+
     if (httpResponseCode>0) {
-      Serial.print("HTTP odpoved: ");
+      Serial.print("HTTP response: ");
       Serial.println(httpResponseCode);
       String payload = http.getString();
       Serial.println(payload);
     } else {
-      Serial.print("Error kod: ");
+      Serial.print("Error code: ");
       Serial.println(httpResponseCode);
     }
     // Free resources
     http.end();
-  } else 
-      Serial.println("Wi-Fi odpojeno");
-
+  } else
+      Serial.println("Wi-Fi disconnected");
 }
 
 void GoToSleep(){
     delay(1);
-  // ESP Deep Sleep 
+  // ESP Deep Sleep
   Serial.println("ESP in sleep mode");
   esp_sleep_enable_timer_wakeup(SLEEP_SEC * 1000000);
   esp_deep_sleep_start();
@@ -75,10 +78,10 @@ void GoToSleep(){
 
 // pripojeni k WiFi | WiFi Connection
 void WiFiConnection(){
-    // Probudit WiFi | Wake up WiFi Modem
-  WiFi.mode( WIFI_STA);
+  // Probudit WiFi | Wake up WiFi Modem
+  WiFi.mode(WIFI_STA);
 
- //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wm;
 
     // reset settings - wipe stored credentials for testing
@@ -86,20 +89,16 @@ void WiFiConnection(){
     //wm.resetSettings();
 
     // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
-    
-    wm.setConfigPortalTimeout(180);   // set portal time to  3 min, then sleep.
+    wm.setConfigPortalTimeout(180);   // set portal time to 3 min, then sleep.
     bool res;
-    res = wm.autoConnect("LaskaKit AutoConnectAP","laskakit"); // password protected ap
+    res = wm.autoConnect("LaskaKitMeteo", "meteostation"); // password protected ap
 
     if(!res) {
         Serial.println("Failed to connect");
         // ESP.restart();
-    } 
+    }
     else {
-        //if you get here you have connected to the WiFi    
+        //if you get here you have connected to the WiFi
         Serial.println("Wi-Fi connected successfully");
     }
 }
@@ -108,9 +107,9 @@ void WiFiConnection(){
 void readBME(){
   temperature = bme.readTemperature();
   humidity    = bme.readHumidity();
-  pressure    = bme.readPressure() / 100.0F;  
-  
-  Serial.print("Temp: "); Serial.print(temperature); Serial.println("°C");
+  pressure    = bme.readPressure() / 100.0F;
+
+  Serial.print("Temperature: "); Serial.print(temperature); Serial.println("°C");
   Serial.print("Humidity: "); Serial.print(humidity); Serial.println("% RH");
   Serial.print("Pressure: "); Serial.print(pressure); Serial.println("hPa");
 }
@@ -120,15 +119,14 @@ void readBME(){
 void readBat(){
   bat_voltage = adc.readVoltage()*deviderRatio;
   Serial.print("Battery voltage " + String(bat_voltage) + "V");
-
 }
 
 void setup() {
   // Hned vypneme WiFi | disable WiFi, coming from DeepSleep, as we do not need it right away
   WiFi.mode( WIFI_OFF );
   delay( 1 );
-  
-  adc.attach(ADC_PIN);  
+
+  adc.attach(ADC_PIN);
 
   Serial.begin(115200);
   while(!Serial) {} // Wait
@@ -150,8 +148,8 @@ void setup() {
   delay(10);
 
   readBME();
-  readBat();     
-  
+  readBat();
+
   // Pripojeni k WiFi | Connect to WiFi
   WiFiConnection();
   postData();
